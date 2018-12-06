21443f8ae653bd5ce197151fe83df575f83f1f8d
Vincent Iroleh <irolehiroleh@gmail.com>
2018-12-06 12:29:21 +0100

    Feature: I setup the app server to listen on port 3000

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+// server 
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('We are live on ' + PORT);
+});
+
