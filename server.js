eb596fb4c014aaf03969966bdd36386e3eaeeb88
Vincent Iroleh <irolehiroleh@gmail.com>
2018-12-06 12:27:16 +0100

    Feature: I used bodyParser to parse in all data from the json.

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ import bodyParser from 'body-parser';
 import redflags from './db/db';
 
 const app = express(); 
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
