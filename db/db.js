a94e69c66402408af5e98649722b730532b02be8
Vincent Iroleh <irolehiroleh@gmail.com>
2018-12-06 12:05:33 +0100

    Created a dummy database to test my APIs

---
+++ db/db.js
@@ -0,0 +1,14 @@
+const redflags = [
+    {
+        id: 1,
+        title:      "Redflag title",
+        details:    "Red flag details"
+    },
+    {
+        id: 2,
+        title :     "Second reflag title",
+        details:    "Second Red flag details"
+    }
+];
+
+export default redflags;
